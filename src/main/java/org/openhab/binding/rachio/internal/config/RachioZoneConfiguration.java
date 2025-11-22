06cf0eb8146bb5c51fe82133109cba5a6e38ae77
dboyett-sketch <dboyett@cox.net>
2025-11-22 15:43:33 -0600

    Update RachioZoneConfiguration.java

--- src/main/java/org/openhab/binding/rachio/internal/config/RachioZoneConfiguration.java
+++ src/main/java/org/openhab/binding/rachio/internal/config/RachioZoneConfiguration.java
@@ -1,1 +1,16 @@
+package org.openhab.binding.rachio.internal.config;
 
+import org.eclipse.jdt.annotation.NonNullByDefault;
+
+/**
+ * The {@link RachioZoneConfiguration} class contains fields mapping thing configuration parameters.
+ *
+ * @author Michael Lobstein - Initial contribution
+ */
+@NonNullByDefault
+public class RachioZoneConfiguration {
+    public String deviceId = "";
+    public String zoneId = "";
+    public int duration = 300;
+    public int refresh = 30;
+}
