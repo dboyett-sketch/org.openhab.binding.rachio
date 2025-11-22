1fbf6ef385f217cd6a34c59c54c2d0fec431dc08
dboyett-sketch <dboyett@cox.net>
2025-11-22 16:18:04 -0600

    Update RachioStatusListener.java

--- src/main/java/org/openhab/binding/rachio/internal/handler/RachioStatusListener.java
+++ src/main/java/org/openhab/binding/rachio/internal/handler/RachioStatusListener.java
@@ -2,6 +2,8 @@ package org.openhab.binding.rachio.internal.handler;
 
 import org.eclipse.jdt.annotation.NonNullByDefault;
 import org.openhab.core.thing.ThingStatus;
+import org.openhab.binding.rachio.internal.api.RachioDevice;
+import org.openhab.binding.rachio.internal.api.RachioZone;
 
 /**
  * The {@link RachioStatusListener} is notified when a Rachio device or zone status changes.
@@ -16,4 +18,6 @@ public interface RachioStatusListener {
     void updateDeviceStatus(ThingStatus status);
 
     void updateZoneStatus(String zoneId, ThingStatus status);
+
+    void onThingStateChanged(RachioDevice device, RachioZone zone);
 }
