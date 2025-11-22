a85d8186e12edd222a8ee4c24015fed363f2a592
dboyett-sketch <dboyett@cox.net>
2025-11-22 15:23:14 -0600

    Update RachioStatusListener.java

--- src/main/java/org/openhab/binding/rachio/internal/handler/RachioStatusListener.java
+++ src/main/java/org/openhab/binding/rachio/internal/handler/RachioStatusListener.java
@@ -1,20 +1,19 @@
 package org.openhab.binding.rachio.internal.handler;
 
-import org.eclipse.jdt.annotation.Nullable;
-import org.openhab.binding.rachio.internal.api.RachioDevice;
-import org.openhab.binding.rachio.internal.api.RachioZone;
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.openhab.core.thing.ThingStatus;
 
 /**
- * The {@link RachioStatusListener} is notified when a device or zone is updated.
+ * The {@link RachioStatusListener} is notified when a Rachio device or zone status changes.
+ *
+ * @author Michael Lobstein - Initial contribution
  */
-public interface RachioStatusListener {
 
-    /**
-     * Called whenever a new device or zone status is received by the cloud handler.
-     *
-     * @param updatedDev   On device updates this is the new RachioDevice information, may be null
-     * @param updatedZone  On zone updates this is the new RachioZone information, may be null
-     * @return true if the update was processed
-     */
-    public boolean onThingStateChanged(@Nullable RachioDevice updatedDev, @Nullable RachioZone updatedZone);
+@NonNullByDefault
+public interface RachioStatusListener {
+    void onRefreshRequested();
+    
+    void updateDeviceStatus(ThingStatus status);
+
+    void updateZoneStatus(String zoneId, ThingStatus status);
 }
