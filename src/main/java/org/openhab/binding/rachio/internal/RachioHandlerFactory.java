739bcb50cb22cc64729fc5946fdd90cc2468788c
dboyett-sketch <dboyett@cox.net>
2025-11-22 15:26:34 -0600

    Update RachioHandlerFactory.java

--- src/main/java/org/openhab/binding/rachio/internal/RachioHandlerFactory.java
+++ src/main/java/org/openhab/binding/rachio/internal/RachioHandlerFactory.java
@@ -1,12 +1,12 @@
 package org.openhab.binding.rachio.internal;
 
+import java.util.Set;
+
 import org.eclipse.jdt.annotation.NonNullByDefault;
 import org.eclipse.jdt.annotation.Nullable;
-import org.openhab.binding.rachio.RachioBindingConstants;
 import org.openhab.binding.rachio.internal.handler.RachioBridgeHandler;
 import org.openhab.binding.rachio.internal.handler.RachioDeviceHandler;
 import org.openhab.binding.rachio.internal.handler.RachioZoneHandler;
-import org.openhab.core.thing.Bridge;
 import org.openhab.core.thing.Thing;
 import org.openhab.core.thing.ThingTypeUID;
 import org.openhab.core.thing.binding.BaseThingHandlerFactory;
@@ -14,27 +14,36 @@ import org.openhab.core.thing.binding.ThingHandler;
 import org.openhab.core.thing.binding.ThingHandlerFactory;
 import org.osgi.service.component.annotations.Component;
 
-@Component(service = ThingHandlerFactory.class, configurationPid = "binding.rachio")
+/**
+ * The {@link RachioHandlerFactory} is responsible for creating things and thing
+ * handlers.
+ *
+ * @author Michael Lobstein - Initial contribution
+ */
+
 @NonNullByDefault
+@Component(service = ThingHandlerFactory.class, configurationPid = "binding.rachio")
 public class RachioHandlerFactory extends BaseThingHandlerFactory {
+    private static final Set<ThingTypeUID> SUPPORTED_THING_TYPES_UIDS = Set.of(THING_TYPE_BRIDGE, THING_TYPE_DEVICE,
+            THING_TYPE_ZONE);
 
     @Override
     public boolean supportsThingType(ThingTypeUID thingTypeUID) {
-        return RachioBindingConstants.SUPPORTED_THING_TYPES_UIDS.contains(thingTypeUID);
+        return SUPPORTED_THING_TYPES_UIDS.contains(thingTypeUID);
     }
 
     @Override
     protected @Nullable ThingHandler createHandler(Thing thing) {
         ThingTypeUID thingTypeUID = thing.getThingTypeUID();
-        
-        if (RachioBindingConstants.THING_TYPE_CLOUD.equals(thingTypeUID)) {
-            return new RachioBridgeHandler((Bridge) thing);
-        } else if (RachioBindingConstants.THING_TYPE_DEVICE.equals(thingTypeUID)) {
+
+        if (THING_TYPE_BRIDGE.equals(thingTypeUID)) {
+            return new RachioBridgeHandler(thing);
+        } else if (THING_TYPE_DEVICE.equals(thingTypeUID)) {
             return new RachioDeviceHandler(thing);
-        } else if (RachioBindingConstants.THING_TYPE_ZONE.equals(thingTypeUID)) {
+        } else if (THING_TYPE_ZONE.equals(thingTypeUID)) {
             return new RachioZoneHandler(thing);
         }
-        
+
         return null;
     }
 }
