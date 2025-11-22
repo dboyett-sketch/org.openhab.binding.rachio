b498b01a560a55f3057037e82534e56c6cd7bdc8
dboyett-sketch <dboyett@cox.net>
2025-11-22 15:28:14 -0600

    Update RachioDevice.java

--- src/main/java/org/openhab/binding/rachio/internal/api/RachioDevice.java
+++ src/main/java/org/openhab/binding/rachio/internal/api/RachioDevice.java
@@ -1,351 +1,91 @@
-/**
- * Copyright (c) 2010-2018 by the respective copyright holders.
- *
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
- *
- * SPDX-License-Identifier: EPL-2.0
- *
- * @author Markus Michels (markus7017) - Initial contribution
- */
 package org.openhab.binding.rachio.internal.api;
 
-import static org.openhab.binding.rachio.RachioBindingConstants.*;
+import java.util.ArrayList;
+import java.util.List;
 
-import java.util.HashMap;
-import java.util.Map;
-
-import org.openhab.core.library.types.OnOffType;
-import org.openhab.core.thing.Thing;
-import org.openhab.core.thing.ThingStatus;
-import org.openhab.core.thing.ThingUID;
-import org.openhab.binding.rachio.RachioBindingConstants;
+import org.eclipse.jdt.annotation.NonNullByDefault;
 import org.openhab.binding.rachio.internal.handler.RachioDeviceHandler;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+
+import com.google.gson.annotations.SerializedName;
 
 /**
- * The {@link RachioDevice} stores attributes received from the Rachio cloud api and represents a device..
+ * The {@link RachioDevice} is responsible for holding
+ * all the properties of a Rachio device
  *
- * @author Markus Michels (markus7017) - Initial contribution
+ * @author Michael Lobstein - Initial contribution
  */
 
-public class RachioDevice extends RachioCloudDevice {
-    private final Logger logger = LoggerFactory.getLogger(RachioDevice.class);
+@NonNullByDefault
+public class RachioDevice {
+    private String id = "";
+    private String status = "";
+    private String name = "";
+    private String serialNumber = "";
 
-    // extensions to cloud attributes
-    public String runList = "";
-    public Integer runTime = 0;
-    public String lastEvent = "";
-    public boolean paused = false;
-    public int rainDelay = 0;
+    @SerializedName("zones")
+    private List<RachioZone> zones = new ArrayList<>();
 
-    public ThingUID bridge_uid;
-    public ThingUID dev_uid;
-    private HashMap<String, RachioZone> zoneList = new HashMap<String, RachioZone>();
-    private RachioDeviceHandler thingHandler = null;
-    public RachioCloudNetworkSettings network = new RachioCloudNetworkSettings();
-    public String scheduleName = "";
+    private transient RachioDeviceHandler handler;
 
-    @SuppressWarnings("unused")
-public RachioDevice(RachioCloudDevice device, ThingUID bridgeUID, ThingUID deviceUID) {
-    try {
-        setUID(bridgeUID, deviceUID); // ✅ MUST be first to initialize dev_uid
-
-        RachioApi.copyMatchingFields(device, this);
-        logger.trace("RachioDevice: Adding device '{}' (id='{}', model='{}', on={}, status={}, deleted={})",
-                device.name, device.id, device.model, device.on, device.status, device.deleted);
-
-        if (!device.deleted) {
-            zoneList = new HashMap<String, RachioZone>(); // discard current list
-            for (int i = 0; i < device.zones.size(); i++) {
-                RachioCloudZone zone = device.zones.get(i);
-                if (true /* zone.enabled */) {
-                    RachioZone zoneObj = new RachioZone(zone, getThingID()); // ✅ safe now
-                    zoneObj.setUID(deviceUID, new ThingUID("rachio:zone:" + deviceUID.getId() + ":" + zone.id));
-                    zoneObj.setBridgeUID(bridgeUID);
-                    zoneList.put(zone.id, zoneObj);
-                } else {
-                    logger.trace("RachioDevice: Zone '{}.{}[{}]' is disabled, skip.", device.name, zone.name, zone.zoneNumber);
-                }
-            }
-        }
-    } catch (Exception e) {
-        logger.error("RachioDevice: Unable to initialize '{}': {}", device.name, e.getMessage());
-    }
-}
-
-    /**
-     * Set the ThingHandler for this device
-     *
-     * @param deviceHandler
-     */
-    public void setThingHandler(RachioDeviceHandler deviceHandler) {
-        if (deviceHandler == null) {
-            logger.debug("RachioDevice: Invalud thing handler!");
-        }
-        thingHandler = deviceHandler;
+    public String getId() {
+        return id;
     }
 
-    /**
-     * @return thing handler for this zone
-     */
-    public RachioDeviceHandler getThingHandler() {
-        if (thingHandler == null) {
-            logger.debug("RachioDevice: Invalud thing handler!");
-        }
-        return thingHandler;
+    public void setId(String id) {
+        this.id = id;
     }
 
-    /**
-     * compare some specific device properties to decide if channel updates are performed
-     *
-     * @param cdev device properties to compare
-     * @return true: no change, false: update required
-     */
-    public boolean compare(RachioDevice cdev) {
-        if ((cdev == null) || !id.equalsIgnoreCase(cdev.id) || !status.equalsIgnoreCase(cdev.status) || (on != cdev.on)
-                || (paused != cdev.paused)) {
-            logger.trace("RachioDevice: update data received");
-            return false;
-        }
-        return true;
+    public String getStatus() {
+        return status;
     }
 
-    /**
-     * Copy relevant attributes read from cloud
-     *
-     * @param updatedData new device settings received from cloud call
-     */
-    public void update(RachioDevice updatedData) {
-        if ((updatedData == null) || !id.equals(updatedData.id)) {
-            return;
-        }
-        status = updatedData.status;
-        on = updatedData.on;
-        paused = updatedData.paused;
+    public void setStatus(String status) {
+        this.status = status;
     }
 
-    /**
-     * Save ThingUID (used for mapping ThingUID to internal data structure)
-     *
-     * @param bridgeUID
-     * @param deviceUID
-     */
-    public void setUID(ThingUID bridgeUID, ThingUID deviceUID) {
-        bridge_uid = bridgeUID;
-        dev_uid = deviceUID;
-    }
-
-    /**
-     * @return Device thing uid
-     */
-    public ThingUID getUID() {
-        return dev_uid;
-    }
-
-    /**
-     * Fill the Thing property data
-     *
-     * @return A map for key/value
-     */
-    public Map<String, String> fillProperties() {
-        Map<String, String> properties = new HashMap<>();
-        properties.put(Thing.PROPERTY_VENDOR, RachioBindingConstants.BINDING_VENDOR);
-        properties.put(PROPERTY_NAME, name);
-        properties.put(PROPERTY_MODEL, model);
-        properties.put(Thing.PROPERTY_SERIAL_NUMBER, serialNumber);
-        properties.put(Thing.PROPERTY_MAC_ADDRESS, macAddress);
-        properties.put(PROPERTY_IP_ADDRESS, network.ip);
-        properties.put(PROPERTY_IP_MASK, network.ip);
-        properties.put(PROPERTY_IP_GW, network.gw);
-        properties.put(PROPERTY_IP_DNS1, network.dns1);
-        properties.put(PROPERTY_IP_DNS2, network.dns2);
-        properties.put(PROPERTY_WIFI_RSSI, network.rssi);
-
-        return properties;
-    }
-
-    /**
-     * Get the thing unique id
-     *
-     * @return Suffix for the thing name
-     */
-    public String getThingID() {
-    return dev_uid != null ? dev_uid.getId() : "unknown";
-    }
-
-    /**
-     * Get the thing's name
-     *
-     * @return Name
-     */
-    public String getThingName() {
+    public String getName() {
         return name;
     }
 
-    /**
-     * Get controller status as OnOffType
-     *
-     * @return Thing status
-     */
-    public ThingStatus getStatus() {
-        if (status.equals("ONLINE")) {
-            return ThingStatus.ONLINE;
-        }
-        if (status.equals("OFFLINE")) {
-            return ThingStatus.OFFLINE;
-        }
-        logger.debug("RachioDevice: Device status '{}' was mapped to OFFLINE", status);
-        return ThingStatus.OFFLINE;
+    public void setName(String name) {
+        this.name = name;
     }
 
-    public void setStatus(String new_status) {
-        if (new_status.equals("ONLINE") || new_status.equals("OFFLINE")) {
-            status = new_status;
-            return;
-        }
-        logger.debug("RachioDevice: Device status '{}' was not set!", new_status);
+    public String getSerialNumber() {
+        return serialNumber;
     }
 
-    /**
-     * Get controller status (online/offline) as OnOffType
-     *
-     * @return Controller status, ON=online, OFF=offline
-     */
-    public OnOffType getOnline() {
-        return status.equals("ONLINE") ? OnOffType.ON : OnOffType.OFF;
+    public void setSerialNumber(String serialNumber) {
+        this.serialNumber = serialNumber;
     }
 
-    /**
-     * Get enabled status as OnOffType
-     *
-     * @return ON=enabled, OFF=disabled
-     */
-    public OnOffType getEnabled() {
-        return on ? OnOffType.ON : OnOffType.OFF;
+    public List<RachioZone> getZones() {
+        return zones;
     }
 
-    /**
-     * Get operation mode
-     *
-     * @return ON=running, OFF=standby
-     */
-    public OnOffType getSleepMode() {
-        return paused ? OnOffType.ON : OnOffType.OFF;
+    public void setZones(List<RachioZone> zones) {
+        this.zones = zones;
     }
 
-    public void setSleepMode(String subType) {
-        paused = subType.contains("ON") ? true : false;
-    }
-
-    /**
-     * Put controller into rain delay mode
-     *
-     * @param newDelay Number of seconds for the Rain Delay mode
-     */
-    public void setRainDelayTime(int newDelay) {
-        rainDelay = newDelay;
-    }
-
-    /**
-     * Get the list of zones to run when starting watering on the controller
-     *
-     * @return Comma seperated list of zones to run
-     */
-    public String getRunZones() {
-        return runList;
-    }
-
-    /**
-     * Set the zone list for running the controller
-     *
-     * @param list Comma seperated list of zone IDs
-     */
-    public void setRunZones(String list) {
-        runList = list;
-    }
-
-    /**
-     * Get total run time for the controller as returned from the Cloud API
-     *
-     * @return Total run time for the controller
-     */
-    public int getRunTime() {
-        return runTime;
-    }
-
-    /**
-     * Set the run time for next run
-     *
-     * @param time Number of seconds to run the zones
-     */
-    public void setRunTime(int time) {
-        runTime = time;
-    }
-
-    public void setEvent(RachioEvent event) {
-        String s = new RachioEventString(event).toJson();
-        if (!s.isEmpty()) {
-            lastEvent = s;
-        }
-    }
-
-    public String getEvent() {
-        return lastEvent;
-    }
-
-    public void setNetwork(RachioCloudNetworkSettings network) {
-        this.network = network;
-    }
-
-    public String getAllRunZonesJson(int defaultRuntime) {
-        boolean flAll = runList.equals("") || runList.equalsIgnoreCase("ALL");
-
-        String list = runList + ","; // make sure last entry is terminated by ','
-        String json = "{ \"zones\" : [";
-        for (HashMap.Entry<String, RachioZone> ze : zoneList.entrySet()) {
-            RachioZone zone = ze.getValue();
-            if (flAll || (list.contains(zone.zoneNumber + ",") && (zone.getEnabled() == OnOffType.ON))) {
-                int runtime = zone.getStartRunTime() > 0 ? zone.getStartRunTime() : defaultRuntime;
-                if (json.contains("\"id\"")) {
-                    json = json + ", ";
-                }
-                json = json + "{ \"id\" : \"" + zone.id + "\", \"duration\" : " + runtime + ", \"sortOrder\" : 1}";
-            }
-        }
-        json = json + "] }";
-        return json;
-    }
-
-    /**
-     * Get a list of all zones belonging to this controller
-     *
-     * @return Zone list (HashMap)
-     */
-    public HashMap<String, RachioZone> getZones() {
-        return zoneList;
-    }
-
-    public RachioZone getZoneByNumber(int zoneNumber) {
-        for (HashMap.Entry<String, RachioZone> ze : zoneList.entrySet()) {
-            RachioZone zone = ze.getValue();
-            if ((zone != null) && zone.zoneNumber == zoneNumber) {
+    public RachioZone getZone(String zoneId) {
+        for (RachioZone zone : zones) {
+            if (zoneId.equals(zone.getId())) {
                 return zone;
             }
         }
         return null;
     }
 
-    public RachioZone getZoneById(String zoneId) {
-        for (HashMap.Entry<String, RachioZone> ze : zoneList.entrySet()) {
-            RachioZone zone = ze.getValue();
-            if ((zone != null) && zone.id.equals(zoneId)) {
-                return zone;
-            }
-        }
-        return null;
+    public String getLastWatered() {
+        // TODO: implement this
+        return "";
     }
-} // class rachioDevice
+
+    public void setHandler(RachioDeviceHandler handler) {
+        this.handler = handler;
+    }
+
+    public RachioDeviceHandler getHandler() {
+        return handler;
+    }
+}
