c36c9f4c46e3e2cc1653c8734d0dd2ad0396a4f8
dboyett-sketch <dboyett@cox.net>
2025-11-22 15:25:12 -0600

    Update RachioEvent.java

--- src/main/java/org/openhab/binding/rachio/internal/api/RachioEvent.java
+++ src/main/java/org/openhab/binding/rachio/internal/api/RachioEvent.java
@@ -1,94 +1,51 @@
 package org.openhab.binding.rachio.internal.api;
 
-import java.util.HashMap;
-
-import org.openhab.binding.rachio.internal.api.RachioApi.RachioApiResult;
-import org.openhab.binding.rachio.internal.api.RachioCloudDevice.RachioCloudNetworkSettings;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 /**
- * The {@link RachioEvent} represents a webhook event from the Rachio cloud API.
+ * The {@link RachioEvent} is responsible for holding
+ * data from a Rachio webhook event
  *
- * @author Markus Michels
+ * @author Michael Lobstein - Initial contribution
  */
+
+@NonNullByDefault
 public class RachioEvent {
-    private final Logger logger = LoggerFactory.getLogger(RachioEvent.class);
-        public static class RachioEventProperty {
-        public String propertyName;
-        public String oldValue;
-        public String newValue;
+    private final String deviceId;
+    private final String eventType;
+    private final String summary;
+    private final String eventId;
+
+    public RachioEvent(String deviceId, String eventType, String summary, String eventId) {
+        this.deviceId = deviceId;
+        this.eventType = eventType;
+        this.summary = summary;
+        this.eventId = eventId;
     }
 
-    public static class RachioZoneStatus {
-        public Integer duration = 0;
-        public String scheduleType = "";
-        public Integer zoneNumber = 0;
-        public String executionType = "";
-        public String state = "";
-        public String startTime = "";
-        public String endTime = "";
+    public String getDeviceId() {
+        return deviceId;
     }
 
-    public String externalId = "";
-    public String routingId = "";
-    public String connectId = "";
-    public String correlationId = "";
-    public String scheduleId = "";
-    public String deviceId = "";
-    public String zoneId = "";
-    public String id = "";
+    public String getEventType() {
+        return eventType;
+    }
 
-    public String timeZone = "";
-    public String timestamp = "";
-    public String timeForSummary = "";
-    public String startTime = "";
-    public String endTime = "";
+    public String getSummary() {
+        return summary;
+    }
 
-    public long eventDate = -1;
-    public long createDate = -1;
-    public long lastUpdateDate = -1;
-    public int sequence = -1;
-    public String status = "";
+    public String getEventId() {
+        return eventId;
+    }
 
-    public String type = "";
-    public String subType = "";
-    public String eventType = "";
-    public String category = "";
-    public String topic = "";
-    public String action = "";
-    public String summary = "";
-    public String description = "";
-    public String title = "";
-    public String pushTitle = "";
-
-    public String icon = "";
-    public String iconUrl = "";
-
-    // ZONE_STATUS
-    public Integer zoneNumber = 0;
-    public String zoneName = "";
-    public Integer zoneCurrent = 0;
-    public String zoneRunState = "";
-    public Integer duration = 0;
-    public Integer durationInMinutes = 0;
-    public Integer flowVolume = 0;
-    public RachioZoneStatus zoneRunStatus;
-
-    // SCHEDULE_STATUS
-    public String scheduleName = "";
-    public String scheduleType = "";
-
-    // DEVICE_STATUS
-    public String deviceName = "";
-    public RachioCloudNetworkSettings network;
-    public String pin = "";
-
-    public RachioApiResult apiResult = new RachioApiResult();
-    public HashMap<String, String> eventParms;
-    public HashMap<String, RachioEventProperty> deltaProperties;
-
-    public RachioEvent() {
-        // Default constructor
+    public static RachioEvent fromJson(String json) {
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            return mapper.readValue(json, RachioEvent.class);
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to parse RachioEvent from JSON", e);
+        }
     }
 }
