c6cadc2b991e2d11bbc9bcafa2b78401a56cb8e8
dboyett-sketch <dboyett@cox.net>
2025-11-24 16:09:00 -0600

    Update RachioApiException.java

--- src/main/java/org/openhab/binding/rachio/internal/api/RachioApiException.java
+++ src/main/java/org/openhab/binding/rachio/internal/api/RachioApiException.java
@@ -11,29 +11,15 @@ import org.eclipse.jdt.annotation.NonNullByDefault;
 public class RachioApiException extends Exception {
     private static final long serialVersionUID = 1L;
 
-    private final RachioApiResult result;
-
-    public RachioApiResult getResult() {
-        return result;
-    }
-
-    public RachioApiException(RachioApiResult result) {
-        super(result.getMessage());
-        this.result = result;
-    }
-
     public RachioApiException(String message) {
         super(message);
-        this.result = new RachioApiResult(false, message);
     }
 
     public RachioApiException(String message, Throwable cause) {
         super(message, cause);
-        this.result = new RachioApiResult(false, message);
     }
 
     public RachioApiException(Throwable cause) {
         super(cause);
-        this.result = new RachioApiResult(false, cause.getMessage());
     }
 }
