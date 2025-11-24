587444f48f7fe349b29ab510e74ea7b083d86300
dboyett-sketch <dboyett@cox.net>
2025-11-24 15:12:41 -0600

    Update RachioDevice.java

--- src/main/java/org/openhab/binding/rachio/internal/api/RachioDevice.java
+++ src/main/java/org/openhab/binding/rachio/internal/api/RachioDevice.java
@@ -1,91 +1,141 @@
 package org.openhab.binding.rachio.internal.api;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.eclipse.jdt.annotation.NonNullByDefault;
-import org.openhab.binding.rachio.internal.handler.RachioDeviceHandler;
-
-import com.google.gson.annotations.SerializedName;
+import org.eclipse.jdt.annotation.Nullable;
 
 /**
- * The {@link RachioDevice} is responsible for holding
- * all the properties of a Rachio device
+ * The {@link RachioDevice} class represents a Rachio irrigation device
  *
- * @author Michael Lobstein - Initial contribution
+ * @author Damion Boyett - Initial contribution
  */
-
 @NonNullByDefault
 public class RachioDevice {
-    private String id = "";
-    private String status = "";
-    private String name = "";
-    private String serialNumber = "";
-
-    @SerializedName("zones")
-    private List<RachioZone> zones = new ArrayList<>();
-
-    private transient RachioDeviceHandler handler;
-
-    public String getId() {
-        return id;
+    // MAKE ALL FIELDS PUBLIC
+    public @Nullable String id;
+    public @Nullable String name;
+    public @Nullable String status;
+    public @Nullable String serialNumber;
+    public @Nullable String model;
+    public @Nullable String macAddress;
+    public @Nullable Double latitude;
+    public @Nullable Double longitude;
+    public @Nullable String timeZone;
+    public @Nullable Boolean on;
+    public @Nullable Boolean deleted;
+    public @Nullable List<RachioZone> zones;
+    
+    // Default constructor
+    public RachioDevice() {
     }
-
-    public void setId(String id) {
+    
+    // GETTER METHODS
+    public @Nullable String getId() { 
+        return id; 
+    }
+    
+    public @Nullable String getName() { 
+        return name; 
+    }
+    
+    public @Nullable String getStatus() { 
+        return status; 
+    }
+    
+    public @Nullable String getSerialNumber() { 
+        return serialNumber; 
+    }
+    
+    public @Nullable String getModel() { 
+        return model; 
+    }
+    
+    public @Nullable String getMacAddress() { 
+        return macAddress; 
+    }
+    
+    public @Nullable Double getLatitude() { 
+        return latitude; 
+    }
+    
+    public @Nullable Double getLongitude() { 
+        return longitude; 
+    }
+    
+    public @Nullable String getTimeZone() { 
+        return timeZone; 
+    }
+    
+    public @Nullable Boolean getOn() { 
+        return on; 
+    }
+    
+    public @Nullable Boolean getDeleted() { 
+        return deleted; 
+    }
+    
+    public @Nullable List<RachioZone> getZones() { 
+        return zones; 
+    }
+    
+    // SETTER METHODS
+    public void setId(@Nullable String id) {
         this.id = id;
     }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
+    
+    public void setName(@Nullable String name) {
         this.name = name;
     }
-
-    public String getSerialNumber() {
-        return serialNumber;
+    
+    public void setStatus(@Nullable String status) {
+        this.status = status;
     }
-
-    public void setSerialNumber(String serialNumber) {
+    
+    public void setSerialNumber(@Nullable String serialNumber) {
         this.serialNumber = serialNumber;
     }
-
-    public List<RachioZone> getZones() {
-        return zones;
+    
+    public void setModel(@Nullable String model) {
+        this.model = model;
     }
-
-    public void setZones(List<RachioZone> zones) {
+    
+    public void setMacAddress(@Nullable String macAddress) {
+        this.macAddress = macAddress;
+    }
+    
+    public void setLatitude(@Nullable Double latitude) {
+        this.latitude = latitude;
+    }
+    
+    public void setLongitude(@Nullable Double longitude) {
+        this.longitude = longitude;
+    }
+    
+    public void setTimeZone(@Nullable String timeZone) {
+        this.timeZone = timeZone;
+    }
+    
+    public void setOn(@Nullable Boolean on) {
+        this.on = on;
+    }
+    
+    public void setDeleted(@Nullable Boolean deleted) {
+        this.deleted = deleted;
+    }
+    
+    public void setZones(@Nullable List<RachioZone> zones) {
         this.zones = zones;
     }
-
-    public RachioZone getZone(String zoneId) {
-        for (RachioZone zone : zones) {
-            if (zoneId.equals(zone.getId())) {
-                return zone;
-            }
-        }
-        return null;
-    }
-
-    public String getLastWatered() {
-        // TODO: implement this
-        return "";
-    }
-
-    public void setHandler(RachioDeviceHandler handler) {
-        this.handler = handler;
-    }
-
-    public RachioDeviceHandler getHandler() {
-        return handler;
+    
+    @Override
+    public String toString() {
+        return "RachioDevice{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", status='" + status + '\'' +
+                ", model='" + model + '\'' +
+                ", on=" + on +
+                '}';
     }
 }
