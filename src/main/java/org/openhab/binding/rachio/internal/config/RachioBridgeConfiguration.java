40115f39ca75324f79a75766ea57ad1e6ef54eaf
dboyett-sketch <dboyett@cox.net>
2025-11-24 14:28:17 -0600

    Update RachioBridgeConfiguration.java

--- src/main/java/org/openhab/binding/rachio/internal/config/RachioBridgeConfiguration.java
+++ src/main/java/org/openhab/binding/rachio/internal/config/RachioBridgeConfiguration.java
@@ -1,1 +1,11 @@
+package org.openhab.binding.rachio.internal.config;
 
+import org.eclipse.jdt.annotation.NonNullByDefault;
+
+@NonNullByDefault
+public class RachioBridgeConfiguration {
+    public String apiKey = "";
+    public String webhookId = "";
+    public int refreshInterval = 60;
+    public String webhookUrl = "";
+}
