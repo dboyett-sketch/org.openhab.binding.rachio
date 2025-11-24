2d1f1b3dac07cf261532e88a19aea2efc8eb61f5
dboyett-sketch <dboyett@cox.net>
2025-11-24 14:29:40 -0600

    Update RachioPerson.java

--- src/main/java/org/openhab/binding/rachio/internal/api/RachioPerson.java
+++ src/main/java/org/openhab/binding/rachio/internal/api/RachioPerson.java
@@ -1,1 +1,14 @@
+package org.openhab.binding.rachio.internal.api;
 
+public class RachioPerson {
+    public String id = "";
+    public String username = "";
+    public String fullName = "";
+    public String email = "";
+    
+    // Add getters if needed
+    public String getId() { return id; }
+    public String getUsername() { return username; }
+    public String getFullName() { return fullName; }
+    public String getEmail() { return email; }
+}
