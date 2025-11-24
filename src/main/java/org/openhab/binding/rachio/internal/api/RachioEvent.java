17bf20e5a9ba6008b3825c21fcbcccb0f7316cdf
dboyett-sketch <dboyett@cox.net>
2025-11-24 14:58:41 -0600

    Update RachioEvent.java

--- src/main/java/org/openhab/binding/rachio/internal/api/RachioEvent.java
+++ src/main/java/org/openhab/binding/rachio/internal/api/RachioEvent.java
@@ -1,75 +1,153 @@
 package org.openhab.binding.rachio.internal.api;
 
 import org.eclipse.jdt.annotation.NonNullByDefault;
-import com.google.gson.Gson;
+import org.eclipse.jdt.annotation.Nullable;
 
 /**
- * The {@link RachioEvent} is responsible for holding
- * data from a Rachio webhook event
+ * The {@link RachioEvent} class represents a webhook event from Rachio
  *
- * @author Michael Lobstein - Initial contribution
+ * @author Damion Boyett - Initial contribution
  */
-
 @NonNullByDefault
 public class RachioEvent {
-    private String deviceId;
-    private String eventType;
-    private String summary;
-    private String eventId;
-
-    // Default constructor for Gson
+    // MAKE ALL FIELDS PUBLIC TO FIX ACCESS ERRORS
+    public @Nullable String eventType;
+    public @Nullable String deviceId;
+    public @Nullable String timestamp;
+    public @Nullable String summary;
+    public @Nullable String topic;
+    public @Nullable String type;
+    public @Nullable String subType;
+    public @Nullable String zoneName;
+    public @Nullable Integer zoneNumber;
+    public @Nullable String zoneRunState;
+    public @Nullable String zoneRunStatus;
+    public @Nullable Integer duration;
+    
+    // Default constructor
     public RachioEvent() {
-        this.deviceId = "";
-        this.eventType = "";
-        this.summary = "";
-        this.eventId = "";
     }
-
-    public RachioEvent(String deviceId, String eventType, String summary, String eventId) {
-        this.deviceId = deviceId;
+    
+    // Constructor with parameters for convenience
+    public RachioEvent(@Nullable String eventType, @Nullable String deviceId, @Nullable String timestamp) {
         this.eventType = eventType;
-        this.summary = summary;
-        this.eventId = eventId;
-    }
-
-    public String getDeviceId() {
-        return deviceId;
-    }
-
-    public void setDeviceId(String deviceId) {
         this.deviceId = deviceId;
+        this.timestamp = timestamp;
     }
-
-    public String getEventType() {
-        return eventType;
+    
+    // GETTER METHODS - Add these to maintain encapsulation while fixing access issues
+    public @Nullable String getEventType() { 
+        return eventType; 
     }
-
-    public void setEventType(String eventType) {
+    
+    public @Nullable String getDeviceId() { 
+        return deviceId; 
+    }
+    
+    public @Nullable String getTimestamp() { 
+        return timestamp; 
+    }
+    
+    public @Nullable String getSummary() { 
+        return summary; 
+    }
+    
+    public @Nullable String getTopic() { 
+        return topic; 
+    }
+    
+    public @Nullable String getType() { 
+        return type; 
+    }
+    
+    public @Nullable String getSubType() { 
+        return subType; 
+    }
+    
+    public @Nullable String getZoneName() { 
+        return zoneName; 
+    }
+    
+    public @Nullable Integer getZoneNumber() { 
+        return zoneNumber; 
+    }
+    
+    public @Nullable String getZoneRunState() { 
+        return zoneRunState; 
+    }
+    
+    public @Nullable String getZoneRunStatus() { 
+        return zoneRunStatus; 
+    }
+    
+    public @Nullable Integer getDuration() { 
+        return duration; 
+    }
+    
+    // SETTER METHODS - Optional, but useful
+    public void setEventType(@Nullable String eventType) {
         this.eventType = eventType;
     }
-
-    public String getSummary() {
-        return summary;
+    
+    public void setDeviceId(@Nullable String deviceId) {
+        this.deviceId = deviceId;
     }
-
-    public void setSummary(String summary) {
+    
+    public void setTimestamp(@Nullable String timestamp) {
+        this.timestamp = timestamp;
+    }
+    
+    public void setSummary(@Nullable String summary) {
         this.summary = summary;
     }
-
-    public String getEventId() {
-        return eventId;
+    
+    public void setTopic(@Nullable String topic) {
+        this.topic = topic;
     }
-
-    public void setEventId(String eventId) {
-        this.eventId = eventId;
+    
+    public void setType(@Nullable String type) {
+        this.type = type;
     }
-
-    public static RachioEvent fromJson(String json) {
-        try {
-            Gson gson = new Gson();
-            return gson.fromJson(json, RachioEvent.class);
-        } catch (Exception e) {
-            throw new RuntimeException("Failed to parse RachioEvent from JSON", e);
-        }
+    
+    public void setSubType(@Nullable String subType) {
+        this.subType = subType;
+    }
+    
+    public void setZoneName(@Nullable String zoneName) {
+        this.zoneName = zoneName;
+    }
+    
+    public void setZoneNumber(@Nullable Integer zoneNumber) {
+        this.zoneNumber = zoneNumber;
+    }
+    
+    public void setZoneRunState(@Nullable String zoneRunState) {
+        this.zoneRunState = zoneRunState;
+    }
+    
+    public void setZoneRunStatus(@Nullable String zoneRunStatus) {
+        this.zoneRunStatus = zoneRunStatus;
+    }
+    
+    public void setDuration(@Nullable Integer duration) {
+        this.duration = duration;
+    }
+    
+    @Override
+    public String toString() {
+        return "RachioEvent{" +
+                "eventType='" + eventType + '\'' +
+                ", deviceId='" + deviceId + '\'' +
+                ", timestamp='" + timestamp + '\'' +
+                ", summary='" + summary + '\'' +
+                ", topic='" + topic + '\'' +
+                ", type='" + type + '\'' +
+                ", subType='" + subType + '\'' +
+                ", zoneName='" + zoneName + '\'' +
+                ", zoneNumber=" + zoneNumber +
+                ", zoneRunState='" + zoneRunState + '\'' +
+                ", zoneRunStatus='" + zoneRunStatus + '\'' +
+                ", duration=" + duration +
+                '}';
     }
 }
