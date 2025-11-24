a6cd9993e611d84847742bdf14b2d8dfc7c9541f
dboyett-sketch <dboyett@cox.net>
2025-11-24 15:59:30 -0600

    Update RachioApiResult.java

--- src/main/java/org/openhab/binding/rachio/internal/api/RachioApiResult.java
+++ src/main/java/org/openhab/binding/rachio/internal/api/RachioApiResult.java
@@ -1,1 +1,37 @@
+package org.openhab.binding.rachio.internal.api;
 
+import org.eclipse.jdt.annotation.NonNullByDefault;
+
+/**
+ * Rachio API Result
+ *
+ * @author Damion Boyett - Initial contribution
+ */
+@NonNullByDefault
+public class RachioApiResult {
+    private final boolean success;
+    private final String message;
+    private final Object data;
+
+    public RachioApiResult(boolean success, String message) {
+        this(success, message, null);
+    }
+
+    public RachioApiResult(boolean success, String message, Object data) {
+        this.success = success;
+        this.message = message;
+        this.data = data;
+    }
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public Object getData() {
+        return data;
+    }
+}
