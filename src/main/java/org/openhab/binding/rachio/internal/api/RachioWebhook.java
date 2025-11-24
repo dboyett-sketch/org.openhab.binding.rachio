d422fea87e9b9938d948d1be6d061c8ffcf41295
dboyett-sketch <dboyett@cox.net>
2025-11-24 14:32:50 -0600

    Update RachioWebhook.java

--- src/main/java/org/openhab/binding/rachio/internal/api/RachioWebhook.java
+++ src/main/java/org/openhab/binding/rachio/internal/api/RachioWebhook.java
@@ -1,1 +1,13 @@
+package org.openhab.binding.rachio.internal.api;
 
+public class RachioWebhook {
+    public String id = "";
+    public String url = "";
+    public String externalId = "";
+    public String eventType = "";
+    
+    public String getId() { return id; }
+    public String getUrl() { return url; }
+    public String getExternalId() { return externalId; }
+    public String getEventType() { return eventType; }
+}
