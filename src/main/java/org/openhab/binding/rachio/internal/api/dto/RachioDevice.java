e157ad1fc22b0a272c5a413cf4858668f3dc6200
dboyett-sketch <dboyett@cox.net>
2025-11-26 20:30:28 -0600

    Update RachioDevice.java

--- src/main/java/org/openhab/binding/rachio/internal/api/dto/RachioDevice.java
+++ src/main/java/org/openhab/binding/rachio/internal/api/dto/RachioDevice.java
@@ -1,1 +1,44 @@
+package org.openhab.binding.rachio.internal.api.dto;
 
+import java.util.List;
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.eclipse.jdt.annotation.Nullable;
+
+/**
+ * Represents a Rachio device details
+ */
+@NonNullByDefault
+public class RachioDevice {
+    public String id = "";
+    public String status = "";
+    public String name = "";
+    public String serialNumber = "";
+    public String model = "";
+    public double latitude;
+    public double longitude;
+    public String timeZone = "";
+    public boolean on;
+    public boolean deleted;
+    @Nullable
+    public List<Zone> zones;
+    @Nullable
+    public ScheduleRule currentSchedule;
+    
+    public static class Zone {
+        public String id = "";
+        public String name = "";
+        public int zoneNumber;
+        public boolean enabled;
+        public int runtime;
+        public int maxRuntime;
+        public String imageUrl = "";
+    }
+    
+    public static class ScheduleRule {
+        public String id = "";
+        public String name = "";
+        public String startDate = "";
+        public int totalDuration;
+        public String status = "";
+    }
+}
