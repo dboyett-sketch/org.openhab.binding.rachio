30f58885732a91e4ec24b830d5195f7708ac8b00
dboyett-sketch <dboyett@cox.net>
2025-11-26 20:33:57 -0600

    Update RachioWebhookEvent.java

--- src/main/java/org/openhab/binding/rachio/internal/api/dto/RachioWebhookEvent.java
+++ src/main/java/org/openhab/binding/rachio/internal/api/dto/RachioWebhookEvent.java
@@ -1,1 +1,42 @@
+package org.openhab.binding.rachio.internal.api.dto;
 
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.eclipse.jdt.annotation.Nullable;
+
+/**
+ * Represents webhook event data from Rachio
+ */
+@NonNullByDefault
+public class RachioWebhookEvent {
+    public String eventType = "";
+    public String deviceId = "";
+    public String timestamp = "";
+    public String summary = "";
+    @Nullable
+    public WebhookDevice device;
+    @Nullable
+    public WebhookZone zone;
+    @Nullable
+    public WebhookNotifier notifier;
+    
+    public static class WebhookDevice {
+        public String id = "";
+        public String name = "";
+        public boolean on;
+        public String status = "";
+    }
+    
+    public static class WebhookZone {
+        public String id = "";
+        public String name = "";
+        public int zoneNumber;
+        public int duration;
+        public String status = "";
+    }
+    
+    public static class WebhookNotifier {
+        public String id = "";
+        public String type = "";
+        public String summary = "";
+    }
+}
