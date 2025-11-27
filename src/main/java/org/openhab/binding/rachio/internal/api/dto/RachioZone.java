31fd948571ac5b0e0b473c358c54fd9c015afe96
dboyett-sketch <dboyett@cox.net>
2025-11-26 20:35:03 -0600

    Update RachioZone.java

--- src/main/java/org/openhab/binding/rachio/internal/api/dto/RachioZone.java
+++ src/main/java/org/openhab/binding/rachio/internal/api/dto/RachioZone.java
@@ -1,1 +1,25 @@
+package org.openhab.binding.rachio.internal.api.dto;
 
+import org.eclipse.jdt.annotation.NonNullByDefault;
+
+/**
+ * Represents zone operations and status
+ */
+@NonNullByDefault
+public class RachioZone {
+    public String zoneId = "";
+    public String name = "";
+    public boolean enabled;
+    public int runtime;
+    public int maxRuntime;
+    public String imageUrl = "";
+    
+    public RachioZone() {}
+    
+    public RachioZone(String zoneId, String name, boolean enabled, int runtime) {
+        this.zoneId = zoneId;
+        this.name = name;
+        this.enabled = enabled;
+        this.runtime = runtime;
+    }
+}
